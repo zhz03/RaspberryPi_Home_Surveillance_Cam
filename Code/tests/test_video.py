fcd99c8c2e454d232ead6e9d74fde75468f99406
Zhaoliang <zhz03@g.ucla.edu>
2021-07-26 00:02:37 -0700

    finish debugging test_video

--- Code/tests/test_video.py
+++ Code/tests/test_video.py
@@ -4,7 +4,7 @@ Created on Sun Jul 25 23:28:47 2021
 
 @author: zhaoliang
 """
-import glob
+
 import os
 from datetime import datetime
 import cv2.cv2 as cv2
@@ -12,9 +12,11 @@ import shutil
 
 current_dir = os.getcwd()
 
-SPACE_LIMIT = 50
+SPACE_LIMIT = 10
 
-def desk_per_Win(path):
+PER_LENGTH = 5
+
+def disk_per_Win(path):
     """
     path: the disk that you want to check, example: path = 'C:'
     return: the percentage of the free space on that disk
@@ -29,6 +31,61 @@ def desk_per_Win(path):
     percent = round(stat[2]/stat[0]*100)
     return percent
 
+def record():
+    """
+    record video
+    return: 
+    """
+    
+    # if you use the external camera, set the right WIDTH and HEIGHT
+    WIDTH = 1280
+    HEIGHT = 720
+    FPS = 24.0
+    
+    # capture video from the default camera
+    cap = cv2.VideoCapture(1)
+    
+    # set the resolution of your camera
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,WIDTH)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,HEIGHT)
+    # set camera frames, if not, by default is 600 
+    cap.set(cv2.CAP_PROP_FPS,FPS)
+    # use MJPG coding, this will work on Windows
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG') 
+    
+    start_time = datetime.now()
+    filename = current_dir +  start_time.strftime('%Y-%m-%d_%H-%M-%S') + '.avi'
+    out = cv2.VideoWriter(filename,fourcc,FPS,(WIDTH,HEIGHT))
+
+    print('recording start')
+    flag = True
+    while flag:
+        # check if the disk space is enough
+        if disk_per_Win('E:') > SPACE_LIMIT:
+            if (datetime.now() - start_time).seconds >= PER_LENGTH:
+                print('start section recording')
+                #logger.info(f'Section recording finish, save file name as {filename}')
+                out.release()
+                # restart recording new vide
+                start_time = datetime.now()
+                filename = current_dir + '/'+ start_time.strftime('%Y-%m-%d_%H-%M-%S') + '.avi'
+                out = cv2.VideoWriter(filename,fourcc,FPS,(WIDTH,HEIGHT))
+                
+            else:
+                ret,frame = cap.read()
+                if ret:
+                    out.write(frame)
+    
+
+            #files = get_files_list(LOCATION + '*.avi')
+            #os.remove(files[0])
+            
+            # print(f'{files[0]} has been deleted')
+            
+    cap.release()
+    
+        
 if __name__ == '__main__':
-    #percent = disk_per()
-    percent = desk_per_Win('E:')
+    #logger.info('start recording')
+    record()
+    
